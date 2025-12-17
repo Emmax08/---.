839d3dba86c926dd73afe264afa5c8e0c9687d17
Emmax08 <benitezbalta61@gmail.com>
2025-12-17 15:51:42 -0600

    Actualizar rpg-mazmorra.js

--- plugins/rpg-mazmorra.js
+++ plugins/rpg-mazmorra.js
@@ -52,7 +52,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
              `┃Tu salud ${evento.health < 0 ? 'bajó en: ' + Math.abs(evento.health) : 'se mantuvo igual.'}\n` +
              `╰━━━━━━━━━━━━⬣`;
 
- await conn.sendFile(m.chat, 'https://files.catbox.moe/ghhsou.jpg', 'mazmorras.jpg', info, m);
+ await conn.sendFile(m.chat, 'https://files.catbox.moe/4rrjeb.png', 'mazmorras.jpg', info, m);
 
 
 
