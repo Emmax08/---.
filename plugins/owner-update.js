bf8b62bb76cc11811329a073ffaa9051ea92a32b
Emmax08 <benitezbalta61@gmail.com>
2025-12-22 10:35:01 -0600

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,25 +1,25 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  const emoji = '😈';
-  const emoji2 = '😈';
-  const emoji4 = '😈';
+  const emoji = '✡️';
+  const emoji2 = '☠️';
+  const emoji4 = '👹';
   const msm = '😈';
 
-  m.reply(`${emoji2} Actualizando el bot...`);
+  m.reply(`${emoji2} Reforzando al pecador...`);
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `${msm} Error al actualizar normalmente. Forzando actualización...`, m);
+      conn.reply(m.chat, `${msm} Error al reforzar normalmente. Forzando el ingreso de poder...`, m);
       exec('git reset --hard origin/main && git pull', (err2, stdout2, stderr2) => {
         if (err2) {
-          conn.reply(m.chat, `${msm} No se pudo forzar la actualización.\nRazón: ${err2.message}`, m);
+          conn.reply(m.chat, `${msm} No se pudo forzar el reforzamiento.\nRazón: ${err2.message}`, m);
           return;
         }
 
         if (stderr2) console.warn(stderr2);
 
-        conn.reply(m.chat, `${emoji} Actualización forzada realizada con éxito.\n\n${stdout2}`, m);
+        conn.reply(m.chat, `${emoji} Reforzamiento forzado realizada con éxito.\n\n${stdout2}`, m);
       });
       return;
     }
@@ -27,9 +27,9 @@ let handler = async (m, { conn }) => {
     if (stderr) console.warn(stderr);
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `${emoji4} El bot ya está actualizado.`, m);
+      conn.reply(m.chat, `${emoji4} El pecador ya tiene suficiente poder.`, m);
     } else {
-      conn.reply(m.chat, `${emoji} Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `${emoji} Reforzamiento de poder éxitoso.\n\n${stdout}`, m);
     }
   });
 };
